9c984c5f14d67264952177f367ab835d9dd5ded6
Radu Maier <ariademur@webserv.co>
2018-12-18 10:41:31 +0100

    init

---
+++ app/src/main/java/co/webserv/android/hawa/app/MainActivity.java
@@ -0,0 +1,12 @@
+package co.webserv.android.hawa.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
