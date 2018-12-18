86ade5a7a4189bca04e526cf2289a9322d21ba8f
Radu Maier <ariademur@webserv.co>
2018-12-18 11:08:41 +0100

    fix MainActivity.java

--- app/src/main/java/co/webserv/android/hawa/app/MainActivity.java
+++ app/src/main/java/co/webserv/android/hawa/app/MainActivity.java
@@ -1,6 +1,14 @@
 package co.webserv.android.hawa.app;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import co.webserv.android.hawa.library.WsActivity;
 
 public class MainActivity extends WsActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
